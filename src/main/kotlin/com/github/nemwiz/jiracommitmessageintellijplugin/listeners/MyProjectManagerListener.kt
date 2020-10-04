c8913347e4e4eb5553b8d939cdb9d4294329290a
GitHub Action <action@github.com>
2020-10-04 03:51:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/nemwiz/jiracommitmessageintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/nemwiz/jiracommitmessageintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,8 +1,8 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.nemwiz.jiracommitmessageintellijplugin.listeners
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.nemwiz.jiracommitmessageintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/nemwiz/jiracommitmessageintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.nemwiz.jiracommitmessageintellijplugin.services
+
+import com.github.nemwiz.jiracommitmessageintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
