4efc6fdba258c1552c6db034c8514ec638ee7d43
Nemanja Ninkovic <nemanja.ninkovic@zuehlke.com>
2023-01-12 11:01:59 +1300

    fix: implements disposable interface for message bus to avoid memory leaks

--- src/main/kotlin/org/nemwiz/jiracommitmessage/handler/BranchCheckoutHandler.kt
+++ src/main/kotlin/org/nemwiz/jiracommitmessage/handler/BranchCheckoutHandler.kt
@@ -1,16 +1,19 @@
 package org.nemwiz.jiracommitmessage.handler
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.vcs.BranchChangeListener
 import com.intellij.openapi.vcs.CheckinProjectPanel
 import com.intellij.openapi.vcs.changes.CommitContext
 import com.intellij.openapi.vcs.checkin.CheckinHandler
 import com.intellij.openapi.vcs.checkin.CheckinHandlerFactory
 import org.nemwiz.jiracommitmessage.listener.BranchCheckoutListener
+import org.nemwiz.jiracommitmessage.services.MyProjectService
 
 class BranchCheckoutHandler : CheckinHandlerFactory() {
     override fun createHandler(panel: CheckinProjectPanel, commitContext: CommitContext): CheckinHandler {
-        val messageBus = panel.project.messageBus.connect()
-        messageBus.subscribe(BranchChangeListener.VCS_BRANCH_CHANGED, BranchCheckoutListener(panel))
+        val service = panel.project.service<MyProjectService>()
+        val messageBus = panel.project.messageBus.connect(service)
+        messageBus.subscribe(BranchChangeListener.VCS_BRANCH_CHANGED, BranchCheckoutListener(panel, service))
         return object : CheckinHandler() {}
     }
 }
--- src/main/kotlin/org/nemwiz/jiracommitmessage/listener/BranchCheckoutListener.kt
+++ src/main/kotlin/org/nemwiz/jiracommitmessage/listener/BranchCheckoutListener.kt
@@ -1,17 +1,18 @@
 package org.nemwiz.jiracommitmessage.listener
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.vcs.BranchChangeListener
 import com.intellij.openapi.vcs.CheckinProjectPanel
 import org.nemwiz.jiracommitmessage.services.MyProjectService
 
-class BranchCheckoutListener(private val panel: CheckinProjectPanel) : BranchChangeListener {
+class BranchCheckoutListener(
+    private val panel: CheckinProjectPanel,
+    private val service: MyProjectService
+) : BranchChangeListener {
 
     override fun branchWillChange(branchName: String) {
     }
 
     override fun branchHasChanged(branchName: String) {
-        val service = panel.project.service<MyProjectService>()
         val commitMessage = service.getTaskIdFromBranchName(branchName)
 
         if (commitMessage !== null) {
