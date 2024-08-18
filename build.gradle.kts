fbb1e175b66a887de5b6a425d27ec70c9f844807
GitHub Action <action@github.com>
2024-08-18 15:53:16 +0200

    chore: bumps the supported version of IntelliJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,7 +91,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
+            untilBuild = providers.gradleProperty("pluginUntilBuild").map { it.ifEmpty { null } }
         }
     }
 
